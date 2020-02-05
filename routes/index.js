477d1a11938752c73bc42db09af130ec01767029
sabrinaromao <romaosabrina14@hotmail.com>
2020-02-05 17:02:44 -0300

    api mysql

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+//Importando a rota de usuários.
+const user = require('./user');
+const router = express.Router();
+
+//Rota para validarmos se a api está atualizada.
+router.get('/api-status', (req, res) =>
+  res.json({
+    status: "ok"
+  })
+);
+//Configurando e mapeando a rota de usuarios. 
+router.use('/user', user);
+
+module.exports = router;
---
+++ server/config/express.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const router = require('../../routes');
+const consign = require('consign');
+const app = express();
+
+consign().include('/controller').into(app);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use('/api/', router);
+module.exports = app;
